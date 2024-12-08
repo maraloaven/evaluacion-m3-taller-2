b08bb41d908bf66c5d3ec019750118783d991dc3
maraloaven <110706790+maraloaven@users.noreply.github.com>
2024-12-08 20:15:26 -0300

    Botón Corregido

--- script.js
+++ script.js
@@ -89,15 +89,4 @@ if (window.location.pathname.includes("contacto.html")) {
             alert("La reserva fue cancelada.");
         }
     });
-
-    const reservarBtn = document.querySelector(".navbar__link--special");
-    reservarBtn.addEventListener("click", () => {
-        const nombre = prompt("¿Cuál es tu nombre?");
-        if (nombre) {
-            alert(`Gracias por reservar con nosotros, ${nombre}.`);
-            console.log(`Reserva realizada por: ${nombre}`);
-        } else {
-            alert("La reserva fue cancelada.");
-        }
-    });
 });
